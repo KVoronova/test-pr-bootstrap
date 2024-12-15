1c312729b3b9a12a8246704d116582949ee75d1f
KVoronova <voronova.vs@gmail.com>
2024-12-15 17:13:13 +0300

    update layout

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'normalize.css';
+import './styles/main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/alert';
